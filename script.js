fb60d01ef7ab2a997f6a5de741ddd48a2b91e052
Denny Rubino <rubidenny@gmail.com>
2024-12-10 19:14:33 +0100

    Add costo biglietto

--- script.js
+++ script.js
@@ -7,5 +7,19 @@ document.getElementById('genera').addEventListener('click', function () {
     const eta = document.getElementById('eta').value;
 }
 
+//CALCOLO PREZZO BIGLIETTO//
+const prezzoKm = 0.21; 
+let costoBiglietto = (km * prezzoKm);
+let offerta = "Biglietto Standard";
+
+if (eta === "minorenne") {
+    costoBiglietto * 0.8; 
+    offerta = "Sconto Minorenne";
+
+} else if (eta === "over65") {
+    costoBiglietto * 0.6; 
+    offerta = "Sconto Over 65";
+}
+
 
 
