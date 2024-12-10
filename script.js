8a829201feeaae7633fbac55b21eec44c48b4b78
Denny Rubino <rubidenny@gmail.com>
2024-12-10 19:13:44 +0100

    Add js element fix css

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//ID ITEMS//
+document.getElementById('genera').addEventListener('click', function () {
+
+    const nome = document.getElementById('nome').value.trim();
+    const km = parseFloat(document.getElementById('km').value);
+    const eta = document.getElementById('eta').value;
+}
+
+
+
